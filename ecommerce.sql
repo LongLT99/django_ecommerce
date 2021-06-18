66e15fe947fc196095dd5798f3ce8b7466dd85a1
longle <longlee1x1@gmail.com>
2021-06-18 16:55:18 +0700

    update database

--- ecommerce.sql
+++ ecommerce.sql
@@ -47,6 +47,7 @@ create table Product (
 ID int(10) NOT NULL AUTO_INCREMENT, 
 product_name varchar(255), 
 product_type varchar(255), 
+`description` text,
 image varchar(255),
 quantity int(10),
 price float(10),
@@ -64,18 +65,10 @@ foreign key (CartID) references Cart(id),
 foreign key (ProductID) references Product(id),
 PRIMARY KEY (ID));
 
-create table `Comment`(
-ID int(10)  NOT NULL AUTO_INCREMENT, 
-Descrip varchar(255),
-ItemID int(10),
-CustomerID int(10),
-foreign key (CustomerID) references Customer(id),
-foreign key (ItemID) references Item(id),
-PRIMARY KEY (ID));
-
 create table Rating(
 ID int(10)  NOT NULL AUTO_INCREMENT, 
 Star int(10),
+`comment` text,
 ItemID int(10),
 CustomerID int(10),
 foreign key (CustomerID) references Customer(id),
@@ -132,6 +125,15 @@ foreign key (EmployeeID) references Employee(id),
 foreign key (OrderID) references `Order`(id),
 PRIMARY KEY (ID));
 
+create table RatingProcess (
+ID int(10) NOT NULL AUTO_INCREMENT, 
+feedback text,
+EmployeeID int(10),
+RatingID int(10),
+foreign key (EmployeeID) references Employee(id),
+foreign key (RatingID) references Rating(id),
+PRIMARY KEY (ID));
+
 INSERT INTO `boec`.`account` (`Username`, `Password`, `Role`) VALUES ('admin', '123456', '0');
 INSERT INTO `boec`.`account` (`Username`, `Password`, `Role`) VALUES ('newguy', '123456', '1');
 
@@ -141,10 +143,10 @@ INSERT INTO `boec`.`customer` (`email`, `tel`, `AccountID`, `FullnameID`, `Addre
 
 INSERT INTO `boec`.`employee` (`name`, `position`, `AccountID`) VALUES ('Nguyen Tuan', 'admin', '1');
 
-INSERT INTO `boec`.`product` (`product_name`, `product_type`, `image`, `quantity`, `price`, `public`) VALUES ('Harry Potter và hòn đá phù thủy', 'Book', 'https://cdn0.fahasa.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/x/nxbtre_thumb_21542017_035423.jpg', '100', '108000', '1');
-INSERT INTO `boec`.`product` (`product_name`, `product_type`, `image`, `quantity`, `price`, `public`) VALUES ('Harry Potter và căn phòng chứa bí mật', 'Book', 'https://cdn0.fahasa.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/x/nxbtre_thumb_21472017_034753.jpg', '80', '	119250', '1');
-INSERT INTO `boec`.`product` (`product_name`, `product_type`, `image`, `quantity`, `price`, `public`) VALUES ('Harry Potter Và Tên Tù Nhân Ngục Azkaban', 'Book', 'https://cdn0.fahasa.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/8/9/8934974148050.jpg', '120', '147600', '1');
-INSERT INTO `boec`.`product` (`product_name`, `product_type`, `image`, `quantity`, `price`, `public`) VALUES ('Harry Potter Và Chiếc Cốc Lửa', 'Book', 'https://cdn0.fahasa.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/h/a/harrypottervachieccoclua.jpg', '70', '221400', '1');
-INSERT INTO `boec`.`product` (`product_name`, `product_type`, `image`, `quantity`, `price`, `public`) VALUES ('Smart Tivi Samsung 4K 55 inch UA55TU8500', 'Electronic', '//cdn.tgdd.vn/Products/Images/1942/219265/ua55tu8500-org.jpg', '50', '16900000', '1');
-INSERT INTO `boec`.`product` (`product_name`, `product_type`, `image`, `quantity`, `price`, `public`) VALUES ('ÁO PHÔNG BÉ GÁI COTTON USA IN NHÂN VẬT', 'Clothes', 'https://media.canifa.com/catalog/product/1/t/1ts21s013-sa534-120-1.jpg', '100', '149000', '1');
+INSERT INTO `boec`.`product` (`product_name`, `product_type`, `description`, `image`, `quantity`, `price`, `public`) VALUES ('Harry Potter và hòn đá phù thủy', 'Book', 'Khi một lá thư được gởi đến cho cậu bé Harry Potter bình thường và bất hạnh, cậu khám phá ra một bí mật đã được che giấu suốt cả một thập kỉ. Cha mẹ cậu chính là phù thủy và cả hai đã bị lời nguyền của Chúa tể Hắc ám giết hại khi Harry mới chỉ là một đứa trẻ, và bằng cách nào đó, cậu đã giữ được mạng sống của mình. Thoát khỏi những người giám hộ Muggle không thể chịu đựng nổi để nhập học vào trường Hogwarts, một trường đào tạo phù thủy với những bóng ma và phép thuật, Harry tình cờ dấn thân vào một cuộc phiêu lưu đầy gai góc khi cậu phát hiện ra một con chó ba đầu đang canh giữ một căn phòng trên tầng ba. Rồi Harry nghe nói đến một viên đá bị mất tích sở hữu những sức mạnh lạ kì, rất quí giá, vô cùng nguy hiểm, mà cũng có thể là mang cả hai đặc điểm trên.', 'https://cdn0.fahasa.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/x/nxbtre_thumb_21542017_035423.jpg', '100', '108000', '1');
+INSERT INTO `boec`.`product` (`product_name`, `product_type`, `description`,`image`, `quantity`, `price`, `public`) VALUES ('Harry Potter và căn phòng chứa bí mật', 'Book', 'Harry khổ sở mong ngóng cho kì nghỉ hè kinh khủng với gia đình Dursley kết thúc. Nhưng một con gia tinh bé nhỏ tội nghiệp đã cảnh báo cho Harry biết về mối nguy hiểm chết người đang chờ cậu ở trường Hogwarts. Trở lại trường học, Harry nghe một tin đồn đang lan truyền về phòng chứa bí mật, nơi cất giữ những bí ẩn đáng sợ dành cho giới phù thủy có nguồn gốc Muggle. Có kẻ nào đó đang phù phép làm tê liệt mọi người, khiến họ gần như đã chết, và một lời cảnh báo kinh hoàng được tìm thấy trên bức tường. Mối nghi ngờ hàng đầu – và luôn luôn sai lầm – là Harry. Nhưng một việc còn đen tối hơn thế đã được hé mở.', 'https://cdn0.fahasa.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/x/nxbtre_thumb_21472017_034753.jpg', '80', '	119250', '1');
+INSERT INTO `boec`.`product` (`product_name`, `product_type`, `description`,`image`, `quantity`, `price`, `public`) VALUES ('Harry Potter Và Tên Tù Nhân Ngục Azkaban', 'Book', 'Harry Potter may mắn sống sót đến tuổi 13, sau nhiều cuộc tấn công của Chúa tể hắc ám. Nhưng hy vọng có một học kỳ yên ổn với Quidditch của cậu đã tiêu tan thành mây khói khi một kẻ điên cuồng giết người hàng loạt vừa thoát khỏi nhà tù Azkaban, với sự lùng sục của những cai tù là giám ngục. Dường như trường Hogwarts là nơi an toàn nhất cho Harry lúc này. Nhưng có phải là sự trùng hợp khi cậu luôn cảm giác có ai đang quan sát mình từ bóng đêm, và những điềm báo của giáo sư Trelawney liệu có chính xác?','https://cdn0.fahasa.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/8/9/8934974148050.jpg', '120', '147600', '1');
+INSERT INTO `boec`.`product` (`product_name`, `product_type`, `description`,`image`, `quantity`, `price`, `public`) VALUES ('Harry Potter Và Chiếc Cốc Lửa', 'Book', 'Harry Potter may mắn sống sót đến tuổi 13, sau nhiều cuộc tấn công của Chúa tể hắc ám. Nhưng hy vọng có một học kỳ yên ổn với Quidditch của cậu đã tiêu tan thành mây khói khi một kẻ điên cuồng giết người hàng loạt vừa thoát khỏi nhà tù Azkaban, với sự lùng sục của những cai tù là giám ngục. Dường như trường Hogwarts là nơi an toàn nhất cho Harry lúc này. Nhưng có phải là sự trùng hợp khi cậu luôn cảm giác có ai đang quan sát mình từ bóng đêm, và những điềm báo của giáo sư Trelawney liệu có chính xác?','https://cdn0.fahasa.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/h/a/harrypottervachieccoclua.jpg', '70', '221400', '1');
+INSERT INTO `boec`.`product` (`product_name`, `product_type`, `description`,`image`, `quantity`, `price`, `public`) VALUES ('Smart Tivi Samsung 4K 55 inch UA55TU8500', 'Electronic', 'Smart Tivi Samsung 4K 55 inch UA55TU8500 thu hút mọi ánh nhìn nhờ thiết kế phần chân đế phẳng chắc chắn, bên cạnh đó với thiết kế không viền 3 cạnh sẽ cho bạn những trải nghiệm giống như đang đứng trước một bức tranh sống động và chân thực.','//cdn.tgdd.vn/Products/Images/1942/219265/ua55tu8500-org.jpg', '50', '16900000', '1');
+INSERT INTO `boec`.`product` (`product_name`, `product_type`, `description`,`image`, `quantity`, `price`, `public`) VALUES ('ÁO PHÔNG BÉ GÁI COTTON USA IN NHÂN VẬT', 'Clothes', 'áo phông cho bé gái chất lượng tốt hình ảnh đẹp','https://media.canifa.com/catalog/product/1/t/1ts21s013-sa534-120-1.jpg', '100', '149000', '1');
 
